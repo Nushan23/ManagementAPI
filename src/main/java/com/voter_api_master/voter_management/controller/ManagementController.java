aa662557ec0aaf4dd97bc0834d895b2e420e6ca5
Nushan23 <45916993+Nushan23@users.noreply.github.com>
2023-01-15 12:46:39 +0530

    Update ManagementController.java

--- src/main/java/com/voter_api_master/voter_management/controller/ManagementController.java
+++ src/main/java/com/voter_api_master/voter_management/controller/ManagementController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/exchanges")
+//@RequestMapping("/api/management")
 
-//@CrossOrigin("*")
+@CrossOrigin("*")
 public class ManagementController {
 
     @Autowired
